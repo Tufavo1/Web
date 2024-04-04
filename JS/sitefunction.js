d502fd2e45a24844fa021cdad3fc6e24800cf5e6
Tufavorito <jo.romeroh@duocuc.cl>
2024-04-04 17:21:16 -0300

    angel listo el anuncio

--- JS/sitefunction.js
+++ JS/sitefunction.js
@@ -1,4 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
+    const slides = document.querySelectorAll('.slide');
+
     var loggedInUser = localStorage.getItem('loggedInUser');
     if (loggedInUser) {
         document.getElementById("user-btn").innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16"><path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/><path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/></svg><span>' + loggedInUser + '</span>';
@@ -65,6 +67,23 @@ document.addEventListener('DOMContentLoaded', function () {
         document.getElementById('registration-form').reset();
         alert('Usuario registrado exitosamente.');
         localStorage.setItem('loggedInUser', email);
-        window.location.href = "HTML/PERFIL.HTML";
+        window.location.href = "HTML/Perfil.html";
+    });
+
+
+    window.addEventListener('scroll', function () {
+        slides.forEach(slide => {
+            const slideTop = slide.offsetTop;
+            const slideBottom = slideTop + slide.offsetHeight;
+
+            const isHalfShown = slideTop < (window.scrollY + window.innerHeight / 2);
+            const isNotScrolledPast = window.scrollY < slideBottom;
+
+            if (isHalfShown && isNotScrolledPast) {
+                slide.classList.add('active');
+            } else {
+                slide.classList.remove('active');
+            }
+        });
     });
 });
